5f27afe132cac0ecd7185082e9180ef9d38ca849
Budi S Han <budihan@users.noreply.github.com>
2018-12-05 17:47:38 +0100

    Added fire mechanics and show own ships

--- public/javascripts/gameboard.js
+++ public/javascripts/gameboard.js
@@ -6,3 +6,82 @@ for (let i = 0; i < 10; i++) {
         $("#r-row-" + i).append("<div class='game-board-square-inactive' id='r-square-" + i + j + "'></div>");
     }
 }
+
+var boardContainer = $("#game-board-left");
+//gameboard as 3d Array
+var gameBoard = [
+                [0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0],
+                [1,0,0,0,0,0,0,0,0,0],
+                [1,0,1,0,0,0,0,0,0,0],
+                [1,0,1,0,1,0,1,0,0,0],
+                [1,0,1,0,1,0,1,0,1,0],
+                [1,0,1,0,1,0,1,0,1,0],
+                ]
+
+boardContainer.click(fire);
+
+showShips();
+function showShips(){
+    for (let i = 0; i < 10;i++){
+        for (let j = 0; j < 10; j++){
+            if (gameBoard[i][j] == 1){
+                $("#l-square-" + i + j).css({"background-color":"gray"});
+            }
+        }
+    }
+}
+
+function fire(e){
+    //e.target (div clicked on)
+    //e.currentTarget parent element of e.target. The element on which the event listener is set on (boardContainer)
+    if (e.target !== e.currentTarget){
+        var row = e.target.id.substring(e.target.id.length-2,e.target.id.length-1);
+        var col = e.target.id.substring(e.target.id.length-1,e.target.id.length);
+
+        console.log("row: " + row + " col: " + col);
+    }
+    // miss
+    if (gameBoard[row][col] == 0) {
+        e.target.style.background = 'blue'
+        gameBoard[row][col] = 3;
+
+    // hit
+    }else if(gameBoard[row][col] == 1) {
+        e.target.style.background = 'red';
+        gameBoard[row][col] = 2;
+
+    // prevent player from firing on fired block
+    }else if(gameBoard[row][col] > 1){
+        alert("Already fired");
+    }
+    e.stopPropagation();
+}
+
+function rotate(e){
+    if (e.target !== e.currentTarget){
+        var row = e.target.id.substring(e.target.id.length-2,e.target.id.length-1);
+        var col = e.target.id.substring(e.target.id.length-1,e.target.id.length);
+
+        console.log("row: " + row + " col: " + col);
+    }
+    // miss
+    if (gameBoard[row][col] == 0) {
+       
+
+    // hit
+    }else if(gameBoard[row][col] == 1) {
+        e.target.style.background = 'red';
+        gameBoard[row][col] = 2;
+        alert("HITTTT");
+
+    // prevent player from firing on fired block
+    }else if(gameBoard[row][col] > 1){
+        alert("Already fired");
+    }
+    e.stopPropagation();
+
+}
