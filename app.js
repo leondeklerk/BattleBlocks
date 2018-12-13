8f1ab6fd5f71e6434c277983fdc8054b06fc04b4
Budi S Han <budihan@users.noreply.github.com>
2018-12-13 13:31:16 +0100

    Game Module

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var WebSocket = require('ws');
 var port = process.argv[2];
 var app = express();
 
+var Game = require("./game");
+
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res) {
@@ -17,8 +19,17 @@ var server = http.createServer(app).listen(port);
 //socket setup
 const wss = new WebSocket.Server({server});
 
+//game setup
+var connectionID = 0;
+var currentGame = new Game();
+
 wss.on('connection', function connection(ws){
     console.log("made socket connection");
+
+    let con = ws;
+    con.id = connectionID++;
+
+    let playerType = currentGame.add
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
       });
